6d57b1889833f8114ece1b211ddeaa256089d4b2
eun1310434 <eun1310434@naver.com>
2018-02-19 19:59:47 +0900

    Initial commit

---
+++ app/src/main/java/com/eun1310434/relativelayout/MainActivity.java
@@ -0,0 +1,19 @@
+/**
+ * 19.02.2018
+ * eun1310434@naver.com
+ * https://blog.naver.com/eun1310434
+ */
+
+package com.eun1310434.relativelayout;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
